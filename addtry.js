9e46a5dc17efb03af6da90693808c41bab144dfc
iliwysfyasbysuoi <61405165+iliwysfyasbysuoi@users.noreply.github.com>
2020-04-27 18:25:32 +0800

    Update addtry.js
    added comments

--- addtry.js
+++ addtry.js
@@ -39,10 +39,10 @@ var restaurant1Details= {
     rType: "Casual Restaurant",
     rCuisine: "Italian",
     rServes: "Pasta, Pizza, Salad, Pastry",
-    rAveFoodRate:10,
-    rAveServiceRate: 9.5 ,
-    rAveEnvironmentRate: 9,
-    rOverallRate: 9.5  
+    rAveFoodRate:10, // take note that this should be the average of all foodratings in review for this particular restuarant (manual computation)
+    rAveServiceRate: 9.5 , // take note that this should be the average of all rAveServiceRate in review for this particular restuarant (manual computation)
+    rAveEnvironmentRate: 9, // take note that this should be the average of all rAveEnvironmentRate in review for this particular restuarant (manual computation)
+    rOverallRate: 9.5  //take note that this should be the average of rAveFoodRate, rAveServiceRate, rAveEnvironmentRate
 };
 
 mongodb.insertOne(restaurantCollection, restaurant1Details);
@@ -56,17 +56,17 @@ var restaurant2Details= {
     rType: "Cafe",
     rCuisine: "Italian",
     rServes: "Coffee, Salad, Pastry",
-    rAveFoodRate: 0,
-    rAveServiceRate: 0 ,
-    rAveEnvironmentRate: 0,
-    rOverallRate: 0 
+    rAveFoodRate: 0, // take note that this should be the average of all foodratings in review for this particular restuarant (manual computation)
+    rAveServiceRate: 0 , // take note that this should be the average of all rAveServiceRate in review for this particular restuarant (manual computation)
+    rAveEnvironmentRate: 0, // take note that this should be the average of all rAveEnvironmentRate in review for this particular restuarant (manual computation)
+    rOverallRate: 0 //take note that this should be the average of rAveFoodRate, rAveServiceRate, rAveEnvironmentRate
 };
 
 mongodb.insertOne(restaurantCollection, restaurant2Details);
 
 // add user1
 var user1Details = {
-    _id: ObjectId(),
+    _id: ObjectId(), // this just generates an _id for this object
     isLoggedIn: false,
     upic: "BobJohnson.jpg",
     uname: "bobjohnson",
@@ -82,7 +82,7 @@ mongodb.insertOne(userCollection, user1Details);
 
 // add user2
 var user2Details = {
-    _id: ObjectId(),
+    _id: ObjectId(), // this just generates an _id for this object
     isLoggedIn: false,
     upic: "HaroldAnderson.jpg",
     uname: "haroldanderson",
@@ -98,18 +98,18 @@ mongodb.insertOne(userCollection, user2Details);
 
 //add review1 created by user1 to restaurant1
 var review1Details = {
-    _id: ObjectId(),
-    authorID: ObjectId(user2Details._id).toString(),
-    rName: user2Details.uname,
-    rPhoto: user2Details.upic,
-    restaurantID: ObjectId(restaurant1Details._id).toString(),
+    _id: ObjectId(), // this just generates an _id for this object
+    authorID: ObjectId(user2Details._id).toString(), // gets _id of user2. this indicates that user2 is the author of the review
+    rName: user2Details.uname, // gets uname of user2.
+    rPhoto: user2Details.upic, // gets upic of user2.
+    restaurantID: ObjectId(restaurant1Details._id).toString(), //gets the _id of restaurant1Details. this indicates that the review is for that restaurant
     restaurantName: "Spicy Heat",
     pubdate: new Date('2020-04-02'),
     votes: 12,
     foodrate: 10,
     servicerate: 9,
     envrate: 8,
-    rOverallRate: 9,
+    rOverallRate: 9, //take note that this should be the average of foodrate, servicerate, envrate
     reviewText: "My wife and I had our last dinner here more than a year ago. My wife and I just remembered the place. The food did not disappoint! Frazzled Cook, especially their Truffle Pasta, is her new favourite. I am from the South but will definitely eat here again!" 
 };
 
@@ -118,17 +118,17 @@ mongodb.insertOne(reviewCollection, review1Details);
 //add review2 created by user1 to restaurant1
 var review2Details = {
     _id: ObjectId(),
-    authorID: ObjectId(user2Details._id).toString(),
-    rName: user2Details.uname,
-    rPhoto: user2Details.upic,
-    restaurantID: ObjectId(restaurant1Details._id).toString(),
+    authorID: ObjectId(user2Details._id).toString(), // gets _id of user2. this indicates that user2 is the author of the review
+    rName: user2Details.uname, // gets uname of user2.
+    rPhoto: user2Details.upic, // gets upic of user2.
+    restaurantID: ObjectId(restaurant1Details._id).toString(), //gets the _id of restaurant1Details. this indicates that the review is for that restaurant
     restaurantName: "Spicy Heat",
     pubdate: new Date('2020-03-26'),
     votes: 40,
     foodrate: 10,
     servicerate: 10,
     envrate: 10,
-    rOverallRate: 10,
+    rOverallRate: 10, //take note that this should be the average of foodrate, servicerate, envrate
     reviewText: " I love everything that has something to do with this restaurant. :)" 
 };
 
